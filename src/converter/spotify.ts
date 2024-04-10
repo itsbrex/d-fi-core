6016ff32865abb9b2d59f4ac10394e5aabaaef6b
itsbrex <roach7@gmail.com>
2024-04-10 14:55:29 -0700

    ✨ feat(spotify-converter): Expand conversion to episodes, shows, and users for Deezer
    ✨ Add functions for converting Spotify episodes, shows, and user data to Deezer format

--- src/converter/spotify.ts
+++ src/converter/spotify.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import SpotifyWebApi from 'spotify-web-api-node';
 import PQueue from 'p-queue';
-import {isrc2deezer, upc2deezer} from './deezer';
+import SpotifyWebApi from 'spotify-web-api-node';
 import type {playlistInfo, trackType} from '../types';
+import {isrc2deezer, upc2deezer} from './deezer';
 
 // type spotifyTypes = 'track' | 'episode' | 'album' | 'artist' | 'playlist' | 'show';
 
@@ -50,6 +50,74 @@ export const setSpotifyAnonymousToken = async () => {
 };
 
 /**
+ * Convert spotify episodes to deezer
+ * @param {String} id Spotify episode id
+ * @returns {trackType}
+ */
+export const episode2deezer = async (id: string) => {
+  const {body} = await spotifyApi.getEpisode(id);
+  return await isrc2deezer(body.name, body.uri.split(':')[2]);
+};
+
+/**
+ * Convert spotify shows to deezer
+ * @param {String} id Spotify show id
+ * @returns {trackType}
+ */
+export const show2deezer = async (id: string) => {
+  const {body} = await spotifyApi.getShow(id);
+  return await isrc2deezer(body.name, body.uri.split(':')[2]);
+};
+
+/**
+ * Convert spotify search to deezer
+ * @param {String} query Spotify search query
+ * @returns {trackType}
+ */
+export const search2deezer = async (query: string) => {
+  const {body} = await spotifyApi.search(query, ['track'], {limit: 1});
+  if (body.tracks && body.tracks.items.length > 0) {
+    const track = body.tracks.items[0];
+    return await isrc2deezer(track.name, track.external_ids.isrc);
+  }
+  throw new Error(`No match on Spotify for query: ${query}`);
+};
+
+/**
+ * Convert spotify userid to deezer
+ * @param {String} id Spotify user id
+ * @returns {trackType}
+ */
+export const user2deezer = async (id: string) => {
+  const {body} = await spotifyApi.getUserPlaylists(id);
+  if (body.items && body.items.length > 0) {
+    const playlist = body.items[0];
+    return await playlist2Deezer(playlist.id);
+  }
+  throw new Error(`No match on Spotify for user: ${id}`);
+};
+
+/**
+ * Get Spotify user's display name
+ * @param {String} id Spotify user id
+ * @returns {String}
+ */
+export const getSpotifyUserName = async (id: string) => {
+  const {body} = await spotifyApi.getUser(id);
+  return body.display_name;
+};
+
+// /**
+//  * Convert spotify local to deezer
+//  * @param {String} id Spotify local id
+//  * @returns {trackType}
+//  */
+// export const local2deezer = async (id: string) => {
+//   const {body} = await spotifyApi.getLocal(id);
+//   return await isrc2deezer(body.name, body.external_ids.isrc);
+// };
+
+/**
  * Convert spotify songs to deezer
  * @param {String} id Spotify track id
  * @returns {trackType}
@@ -78,11 +146,11 @@ export const playlist2Deezer = async (
 ): Promise<[playlistInfo, trackType[]]> => {
   const {body} = await spotifyApi.getPlaylist(id);
   const tracks: trackType[] = [];
-  let items = body.tracks.items;
+  let {items} = body.tracks;
   let offset = getOffset(body.tracks.next);
 
   while (offset !== 0) {
-    const {body} = await spotifyApi.getPlaylistTracks(id, {limit: 100, offset: offset ? offset : 0});
+    const {body} = await spotifyApi.getPlaylistTracks(id, {limit: 100, offset: offset || 0});
     offset = getOffset(body.next);
     items = [...items, ...body.items];
   }
